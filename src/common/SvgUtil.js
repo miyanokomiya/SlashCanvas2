17ced213c5ae452058fe9e8249d2fc1d1f15cb5a
miyanokomiya <tomoya.hstexze24632@gmail.com>
2016-08-16 22:25:01 +0900

    SVGの省略コマンドに対応

--- src/common/SvgUtil.js
+++ src/common/SvgUtil.js
@@ -62,7 +62,8 @@ define(function(require) {
 				ret.push({
 					tag : "path",
 					pointList : this.parsePath(tagPathList[i]),
-					style : this.parseTagStyle(tagPathList[i])
+					style : this.parseTagStyle(tagPathList[i]),
+					close : tagPathList[i].attributes.d.value.match(/Z|z/)
 				});
 			}
 
@@ -72,7 +73,8 @@ define(function(require) {
 				ret.push({
 					tag : "rect",
 					pointList : this.parseRect(tagRectList[i]),
-					style : this.parseTagStyle(tagRectList[i])
+					style : this.parseTagStyle(tagRectList[i]),
+					close : true
 				});
 			}
 
@@ -82,7 +84,8 @@ define(function(require) {
 				ret.push({
 					tag : "ellipse",
 					pointList : this.parseEllipse(tagEllipseList[i]),
-					style : this.parseTagStyle(tagEllipseList[i])
+					style : this.parseTagStyle(tagEllipseList[i]),
+					close : true
 				});
 			}
 
@@ -92,7 +95,8 @@ define(function(require) {
 				ret.push({
 					tag : "circle",
 					pointList : this.parseCircle(tagCircleList[i]),
-					style : this.parseTagStyle(tagCircleList[i])
+					style : this.parseTagStyle(tagCircleList[i]),
+					close : true
 				});
 			}
 
@@ -274,7 +278,7 @@ define(function(require) {
 		 * pathタグを解析する
 		 * @method parsePath
 		 * @param svgPath {} SVGのpathタグDOM
-		 * @return {point[]} {x,y}の座標リスト
+		 * @return {vector[]} 座標リスト
 		 */
 		parsePath : function(svgPath) {
 			var ret = [];
@@ -289,220 +293,201 @@ define(function(require) {
 			for (var i = 0; i < elementList.length; i++) {
 				var current = elementList[i];
 				var pList = [];
+
 				var b0 = null;
 				var b1 = null;
 				var b2 = null;
 				var b3 = null;
 				var centers = null;
 
-				switch (current.command) {
+				switch (current[0]) {
 					case "M":
-						// 初期位置(絶対)
-						pList.push({x : current.x, y : current.y});
-						break;
-					case "m":
-						// 初期位置(相対)
-						pList.push({x : pastVec.x + current.x, y : pastVec.y + current.y});
-						break;
 					case "L":
 					case "H":
-					case "V":
 						// 直線(絶対)
-						pList.push({x : current.x, y : current.y});
+						pList.push({x : parseFloat(current[1], 10), y : parseFloat(current[2], 10)});
 						break;
+					case "m":
 					case "l":
 					case "h":
-					case "v":
 						// 直線(相対)
-						pList.push({x : pastVec.x + current.x, y : pastVec.y + current.y});
+						pList.push({x : pastVec.x + parseFloat(current[1], 10), y : pastVec.y + parseFloat(current[2], 10)});
+						break;
+					case "V":
+						// 垂直(絶対)
+						pList.push({x : pastVec.x, y : parseFloat(current[1], 10)});
+						break;
+					case "v":
+						// 垂直(相対)
+						pList.push({x : pastVec.x, y : pastVec.y + parseFloat(current[1], 10)});
 						break;
 					case "Q" :
 						// 制御点準備
 						b0 = pastVec;
 						b1 = {
-							x : current.x,
-							y : current.y
+							x : parseFloat(current[1], 10),
+							y : parseFloat(current[2], 10)
 						};
 						b2 = {
-							x : current.x2,
-							y : current.y2
+							x : parseFloat(current[3], 10),
+							y : parseFloat(current[4], 10)
 						};
 						// 近似
 						pList = mathUtil.approximateBezier([b0, b1, b2], this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b2;
 						break;
 					case "q" :
 						// 制御点準備
 						b0 = pastVec;
 						b1 = {
-							x : b0.x + current.x,
-							y : b0.y + current.y
+							x : b0.x + parseFloat(current[1], 10),
+							y : b0.y + parseFloat(current[2], 10)
 						};
 						b2 = {
-							x : b0.x + current.x2,
-							y : b0.y + current.y2
+							x : b0.x + parseFloat(current[3], 10),
+							y : b0.y + parseFloat(current[4], 10)
 						};
 						// 近似
 						pList = mathUtil.approximateBezier([b0, b1, b2], this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b2;
 						break;
 					case "T" :
 						// 制御点準備
 						b0 = pastVec;
 						b1 = mathUtil.symmetryPoint2D(b0, pastControlVec);
 						b2 = {
-							x : current.x,
-							y : current.y
+							x : parseFloat(current[1], 10),
+							y : parseFloat(current[2], 10)
 						};
 						// 近似
 						pList = mathUtil.approximateBezier([b0, b1, b2], this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b2;
 						break;
 					case "t" :
 						// 制御点準備
 						b0 = pastVec;
 						b1 = mathUtil.symmetryPoint2D(b0, pastControlVec);
 						b2 = {
-							x : b0.x + current.x,
-							y : b0.y + current.y
+							x : b0.x + parseFloat(current[1], 10),
+							y : b0.y + parseFloat(current[2], 10)
 						};
 						// 近似
 						pList = mathUtil.approximateBezier([b0, b1, b2], this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b2;
 						break;
 					case "C" :
 						// 制御点準備
 						b0 = pastVec;
 						b1 = {
-							x : current.x,
-							y : current.y
+							x : parseFloat(current[1], 10),
+							y : parseFloat(current[2], 10)
 						};
 						b2 = {
-							x : current.x2,
-							y : current.y2
+							x : parseFloat(current[3], 10),
+							y : parseFloat(current[4], 10)
 						};
 						b3 = {
-							x : current.x3,
-							y : current.y3
+							x : parseFloat(current[5], 10),
+							y : parseFloat(current[6], 10)
 						};
 						// 近似
 						pList = mathUtil.approximateBezier([b0, b1, b2, b3], this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b3;
 						break;
 					case "c" :
 						// 制御点準備
 						b0 = pastVec;
 						b1 = {
-							x : b0.x + current.x,
-							y : b0.y + current.y
+							x : b0.x + parseFloat(current[1], 10),
+							y : b0.y + parseFloat(current[2], 10)
 						};
 						b2 = {
-							x : b0.x + current.x2,
-							y : b0.y + current.y2
+							x : b0.x + parseFloat(current[3], 10),
+							y : b0.y + parseFloat(current[4], 10)
 						};
 						b3 = {
-							x : b0.x + current.x3,
-							y : b0.y + current.y3
+							x : b0.x + parseFloat(current[5], 10),
+							y : b0.y + parseFloat(current[6], 10)
 						};
 						// 近似
 						pList = mathUtil.approximateBezier([b0, b1, b2, b3], this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b3;
 						break;
 					case "S" :
 						// 制御点準備
 						b0 = pastVec;
 						b1 = mathUtil.symmetryPoint2D(b0, pastControlVec);
 						b2 = {
-							x : current.x,
-							y : current.y
+							x : parseFloat(current[1], 10),
+							y : parseFloat(current[2], 10)
 						};
 						b3 = {
-							x : current.x2,
-							y : current.y2
+							x : parseFloat(current[3], 10),
+							y : parseFloat(current[4], 10)
 						};
 						// 近似
 						pList = mathUtil.approximateBezier([b0, b1, b2, b3], this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b3;
 						break;
 					case "s" :
 						// 制御点準備
 						b0 = pastVec;
 						b1 = mathUtil.symmetryPoint2D(b0, pastControlVec);
 						b2 = {
-							x : b0.x + current.x,
-							y : b0.y + current.y
+							x : b0.x + parseFloat(current[1], 10),
+							y : b0.y + parseFloat(current[2], 10)
 						};
 						b3 = {
-							x : b0.x + current.x2,
-							y : b0.y + current.y2
+							x : b0.x + parseFloat(current[3], 10),
+							y : b0.y + parseFloat(current[4], 10)
 						};
 						// 近似
 						pList = mathUtil.approximateBezier([b0, b1, b2, b3], this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b3;
 						break;
 					case "A":
 						b0 = pastVec;
 						b1 = {
-							x : current.x,
-							y : current.y
+							x : parseFloat(current[6], 10),
+							y : parseFloat(current[7], 10)
 						};
 
 						pList = mathUtil.approximateArcWithPoint(
-							current.rx, current.ry,
+							parseFloat(current[1], 10), parseFloat(current[2], 10),
 							b0,
 							b1,
-							current.largeArcFlag,
-							current.sweepFlag,
-							current.xAxisRotation / 180 * Math.PI,
+							parseInt(current[4], 10),
+							parseInt(current[5], 10),
+							parseFloat(current[3], 10) / 180 * Math.PI,
 							this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b1;
 						break;
 					case "a":
 						b0 = pastVec;
 						b1 = {
-							x : b0.x + current.x,
-							y : b0.y + current.y
+							x : b0.x + parseFloat(current[6], 10),
+							y : b0.y + parseFloat(current[7], 10)
 						};
 
 						pList = mathUtil.approximateArcWithPoint(
-							current.rx, current.ry,
+							parseFloat(current[1], 10), parseFloat(current[2], 10),
 							b0,
 							b1,
-							current.largeArcFlag,
-							current.sweepFlag,
-							current.xAxisRotation / 180 * Math.PI,
+							parseInt(current[4], 10),
+							parseInt(current[5], 10),
+							parseFloat(current[3], 10) / 180 * Math.PI,
 							this.BEZIER_SPLIT_COUNT);
 						// 始点は前回点なので除去
 						pList.shift();
-						// 終点は誤差を除去
-						pList[pList.length - 1] = b1;
 						break;
 					default:
 						// 無視して次へ
@@ -530,95 +515,64 @@ define(function(require) {
 		 * pathタグd属性文字列を分割する
 		 * @method splitD
 		 * @param dString {string} pathのd要素文字列
-		 * @return {[]} {command, x, y}要素リスト
+		 * @return {string[][]} コマンド単位の情報配列の配列
 		 */
 		splitD : function(dString) {
 			var ret = [];
 
-			// 全コマンドリスト
-			var reg = /M|m|L|l|H|h|V|v|C|c|S|s|Q|q|T|t|A|a|Z|z|B|b|R|r/;
-			// 水平コマンド
-			var hLineReg = /H|h/;
-			// 垂直コマンド
-			var vLineReg = /V|v/;
-			// 座標１つ以上のコマンド
-			var onePointReg = /M|m|L|l|T|t|Q|q|C|c/;
-			// 座標２つ以上のコマンド
-			var twoPointReg = /Q|q|C|c|S|s/;
-			// 座標3つ以上のコマンド
-			var threePointReg = /C|c/;
+			// 全コマンドリスト(BbRr非対応)
+			var allCommand = /M|m|L|l|H|h|V|v|C|c|S|s|Q|q|T|t|A|a|Z|z/;
 
-			// 円弧コマンド
-			var arcReg = /A|a/;
+			// 要素分割
+			var strList = dString.trim().split(/,| /);
+			// 空要素削除(カンマとスペースが続くと空が入る)
+			strList = strList.filter(function(str) {
+				return str !== "";
+			});
 
-			// 残り文字列
-			var current = dString;
+			// 直前のコマンド
+			var pastCommand = "M";
 
-			while (current.length > 0) {
-				var element = {};
-				// コマンド部分)
-				element.command = current[0];
-				current = current.slice(1);
-
-				// このブロックの情報部分
-				var block = null;
-
-				// 次の要素開始位置
-				var index = current.search(reg);
-				if (index === -1) {
-					// 次の要素なし
-					block = current.slice(0);
-					current = "";
+			for (var i = 0; i < strList.length;) {
+				var info = [];
+				// コマンドがあるか？
+				if (strList[i].match(allCommand)) {
+					// あるので回収
+					info[0] = strList[i].trim();
+					pastCommand = info[0];
+					// 進む
+					i++;
 				} else {
-				block = current.slice(0, index);
-					current = current.slice(index);
+					// 前回同様
+					info[0] = pastCommand;
 				}
 
-				// 要素分割
-				var strList = block.trim().split(/,| /);
-				// 空要素削除(カンマとスペースが続くと空が入る)
-				strList = strList.filter(function(str) {
-					return str;
-				})
-
-				if (element.command.search(hLineReg) !== -1) {
-					// 水平
-					element.x = parseFloat(strList[0], 10);
-					element.y = 0;
-				} else if (element.command.search(vLineReg) !== -1) {
-					// 垂直
-					element.x = 0;
-					element.y = parseFloat(strList[0], 10);
-				} else if (element.command.search(arcReg) !== -1) {
-					// 円弧
-					element.rx = parseFloat(strList[0], 10);
-					element.ry = parseFloat(strList[1], 10);
-					element.xAxisRotation = parseFloat(strList[2], 10);
-					element.largeArcFlag = parseInt(strList[3], 10);
-					element.sweepFlag = parseInt(strList[4], 10);
-					element.x = parseFloat(strList[5], 10);
-					element.y = parseFloat(strList[6], 10);
-				} else {
-					// 座標１つ
-					if (element.command.search(onePointReg) !== -1) {
-						element.x = parseFloat(strList[0], 10);
-						element.y = parseFloat(strList[1], 10);
-
-						// 座標２つ
-						if (element.command.search(twoPointReg) !== -1) {
-							element.x2 = parseFloat(strList[2], 10);
-							element.y2 = parseFloat(strList[3], 10);
-
-							// 座標3つ
-							if (element.command.search(threePointReg) !== -1) {
-								element.x3 = parseFloat(strList[4], 10);
-								element.y3 = parseFloat(strList[5], 10);
-							}
-						}
-					}
+				// 情報数で場合分け
+				if (info[0].match(/Z|z/)) {
+					// 0つ
+				} else if (info[0].match(/V|v|H|h/)) {
+					// 2つ
+					info = info.concat(strList.slice(i, i + 2));
+					i += 2;
+				} else if (info[0].match(/M|m|L|l|T|t/)) {
+					// 2つ
+					info = info.concat(strList.slice(i, i + 2));
+					i += 2;
+				} else if (info[0].match(/Q|q|S|s/)) {
+					// 4つ
+					info = info.concat(strList.slice(i, i + 4));
+					i += 4;
+				} else if (info[0].match(/C|c/)) {
+					// 6つ
+					info = info.concat(strList.slice(i, i + 6));
+					i += 6;
+				} else if (info[0].match(/A|a/)) {
+					// 7つ
+					info = info.concat(strList.slice(i, i + 7));
+					i += 7;
 				}
 
-				ret.push(element);
+				ret.push(info);
 			}
 
 			return ret;
